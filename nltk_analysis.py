e9635b264c10bb00913036045babb3831d69caa7
Chen Yitong <cheny2@eduroam-175-245.dyn.carleton.edu>
2018-05-22 19:22:11 -0500

    10-fold cross validation

--- nltk_analysis.py
+++ nltk_analysis.py
@@ -2,12 +2,14 @@
 Analyze the Dakota data using the NLTK library
 
 Author: Yitong Chen 
-Last Updated: 05/01/2018
+Last Updated: 05/22/2018
 """
 from nltk import *
-import matplotlib.pyplot as plt
+from math import *
+from sklearn.model_selection import KFold
 import re
 import collections
+import numpy as np
 
 def create_word_list(raw_data):
     """Create a list of words from the input data.
@@ -45,20 +47,16 @@ def unigram_analysis_word(words):
     """
     fwords = FreqDist(words)
     fdwords = SimpleGoodTuringProbDist(fwords)
-    common = fwords.most_common(21)
+    common = fwords.most_common(16)
     idx = 0
     # Remove the count of the line breaker
-    while idx < 21:
+    while idx < 16:
         if common[idx][0] == "<line>":
             common.remove(common[idx])
             break
         idx += 1
         
-    print(common)
-    #    fwords.plot(20, cumulative=False)
-    print("----------------")
-    print("Probability of '" + common[0][0] + "': ", fdwords.prob(common[0]))
-    print("\n")
+    return common, fdwords
     
 
 def multigram_analysis_word(words, n):
@@ -83,15 +81,15 @@ def multigram_analysis_word(words, n):
         if not "<line>" in entry[0]:
             common_filtered.append(entry)
             counter += 1
-    print(len(common_filtered))
             
-    print(common)
-    print(common_filtered)
-    print("----------------")
-    most_common = " ".join(common_filtered[0][0])
-    print("Probability of '" + most_common + "': ", fdgram.prob(most_common))
-    print("\n")
+#    print(common)
+#    print(common_filtered)
+#    print("----------------")
+#    most_common = " ".join(common_filtered[0][0])
+#    print("Probability of '" + most_common + "': ", fdgram.prob(most_common))
+#    print("\n")
 
+    return common_filtered, fdgram
 
 def count_inversion(lst):
     """Helper function to count inversions.
@@ -99,21 +97,155 @@ def count_inversion(lst):
     Parameters:
     lst - the list to be analyzed
     
-    Returns: number of inversions
+    Returns: sorted list and number of inversions
     """
-    pass
+    if len(lst) == 1:
+        return lst, 0
+    else:
+        left, left_count = count_inversion(lst[:len(lst)//2])
+        right, right_count = count_inversion(lst[len(lst)//2:])
+        inter, inter_count = merge_and_count(left, right)
+        return inter, left_count + right_count + inter_count
+
+
+def merge_and_count(lst1, lst2):
+    """Helper function to merge two lists and count the 
+    number of inversions across the two lists.
     
+    Parameters:
+    lst1 - list of integers
+    lst2 - another list of integers
+    
+    Returns: sorted list and number of inversions
+    """
+    a = 0
+    b = 0
+    count = 0
+    sorted_list = []
+    while a < len(lst1) and b < len(lst2):
+        if lst1[a] <= lst2[b]:
+            sorted_list.append(lst1[a])
+            a += 1
+        else:
+            sorted_list.append(lst2[b])
+            count += len(lst1) - a
+            b += 1
+    if a < len(lst1):
+        for i in range(a, len(lst1)):
+            sorted_list.append(lst1[i])
+    else: 
+        for i in range(b, len(lst2)):
+            sorted_list.append(lst2[i])
+            
+    return sorted_list, count
+    
+    
+def cross_validate_uni(word_train, word_test, uni_scores, uni_prob_scores):
+    """Cross validate for unigrams.
+    
+    Parameters:
+    word_train - a list of words as training data
+    word_test - a list of words as testing data
+    uni_score - a list of scores based on inversions
+    uni_prob_score - a list of scores based on probability
+    """
+    # 10-fold test on unigram
+    uni_train, fdtrain = unigram_analysis_word(word_train)
+    uni_test, fdtest = unigram_analysis_word(word_test)        
+    uni_test_list = [entry[0] for entry in uni_test]
+    inversion = []
+    for entry in uni_train:
+        if entry[0] in uni_test_list:            
+            inversion.append(uni_test_list.index(entry[0]))
+        else:
+            inversion.append(10)
+    sorted, score = count_inversion(inversion)
+    uni_scores.append(score)
+    uni_most_common = uni_train[0][0]
+    uni_prob_most_common_train = fdtrain.prob(uni_most_common)
+    uni_prob_most_common_test = fdtest.prob(uni_most_common)     
+    uni_prob_scores.append(abs(uni_prob_most_common_train - uni_prob_most_common_test))
+
+    
+def cross_validate_multi(word_train, word_test, multi_score, multi_prob_score, n):
+    """Cross validate for multigrams.
+    
+    Parameters:
+    word_train - a list of words as training data
+    word_test - a list of words as testing data
+    uni_score - a list of scores based on inversions
+    uni_prob_score - a list of scores based on probability
+    """
+    # 10-fold test on unigram
+    multi_train, fdtrain = multigram_analysis_word(word_train, n)
+    multi_test, fdtest = multigram_analysis_word(word_test, n)        
+    multi_test_list = [entry[0] for entry in multi_test]
+    inversion = []
+    for entry in multi_train:
+        if entry[0] in multi_test_list:            
+            inversion.append(multi_test_list.index(entry[0]))
+        else:
+            inversion.append(10)
+    sorted, score = count_inversion(inversion)
+    multi_score.append(score)
+    multi_most_common = multi_train[0][0]
+    multi_prob_most_common_train = fdtrain.prob(multi_most_common)
+    multi_prob_most_common_test = fdtest.prob(multi_most_common)     
+    multi_prob_score.append(abs(multi_prob_most_common_train - multi_prob_most_common_test))
+    
+    
+def cross_validate(words):
+    """Run 10-fold cross-validation on the data.
+    Result of the test implies the reliability of the ngram analysis.
+    
+    Parameters:
+    words - the list of dakota words
+    """
+    word_array = np.array(words)
+    # Define 5 split fold
+    kf = KFold(n_splits=10, shuffle=True)
+    kf.get_n_splits(word_array)
+    uni_scores = []
+    uni_prob_scores = []
+    bi_scores = []
+    bi_prob_scores = []
+    tri_scores = []
+    tri_prob_scores = []
+    # Create train and test sets and run the cross validation
+    for train_index, test_index in kf.split(word_array):
+        word_train, word_test = word_array[train_index], word_array[test_index]
+        cross_validate_uni(word_train, word_test, uni_scores, uni_prob_scores)
+        cross_validate_multi(word_train, word_test, bi_scores, bi_prob_scores, 2)
+        cross_validate_multi(word_train, word_test, tri_scores, tri_prob_scores, 3)
+    
+    print("------------------------*------------------------")
+    print("The standard deviataion of the unigram score is", np.std(uni_scores))
+    print("The mean of the unigram score is", np.mean(uni_scores))
+    print("The standard deviataion of the unigram probability score is", np.std(uni_prob_scores))
+    print("------------------------*------------------------")
+    print("The standard deviataion of the bigram score is", np.std(bi_scores))
+    print("The mean of the bigram score is", np.mean(bi_scores))
+    print("The standard deviataion of the bigram probability score is", np.std(bi_prob_scores))
+    print("------------------------*------------------------")
+    print("The standard deviataion of the trigram score is", np.std(tri_scores))
+    print("The mean of the trigram score is", np.mean(tri_scores))
+    print("The standard deviataion of the trigram probability score is", np.std(tri_prob_scores))
+    print("------------------------*------------------------")
+     
     
 def main():
     # Open the raw Dakota data
-    raw_data = open("test_data.csv", "r")
+    raw_data = open("dakota.csv", "r")
     words = create_word_list(raw_data)
     # Run analysis on word-level with 10-fold probability test
-    unigram_analysis_word(words)
-    for i in range(2, 4):
-        multigram_analysis_word(words, i)
+#    unigram_analysis_word(words)
+#    for i in range(2, 4):
+#        multigram_analysis_word(words, i)
     # Run analysis on morpheme level
     
+    # Cross validation test
+    cross_validate(words)
+
     raw_data.close()
     
     
